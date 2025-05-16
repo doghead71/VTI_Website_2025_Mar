4ca88de42c7eb815887a9aa9f801f75afa9fa927
oshayne meggoe <oshaynemeggoe6@gmail.com>
2025-05-15 20:12:54 -0500

    update

--- website_oshayne/scripts/register.js
+++ website_oshayne/scripts/register.js
@@ -4,6 +4,29 @@ function CreateRegister() {
     const email = document.querySelector('input[name="email"]').value;
     const password = document.querySelector('input[name="password"]').value;
 
+    //perform validation
+    if (!email || !password) {
+        alert("Please fill in all fields.");
+        return;
+    }
+    if (password.length < 8) {
+        alert("Password must be at least 8 characters long.");
+        return;
+    }
+    if(!email.includes("@")) {
+        alert("Please enter a valid email address.");
+        return;
+    }
+    if(!email.includes(".")) {
+        alert("Please enter a valid email address.");
+        return;
+    }
+    const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    if (!passwordPattern.test(password)) {
+        alert("Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.");
+        return;
+    }
+    
     // here you would typically send the data to your server
     console.log("Email: ", email);
     console.log("Password: ", password);
