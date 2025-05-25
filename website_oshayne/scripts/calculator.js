22bd589603faca58327cc69072d6924310fa8418
oshayne meggoe <oshaynemeggoe6@gmail.com>
2025-05-24 19:16:52 -0500

    update

--- website_oshayne/scripts/calculator.js
+++ website_oshayne/scripts/calculator.js
@@ -7,9 +7,9 @@ function appendToDisplay(input) {
 }
 
 function clearDisplay() {
-    display.value = "";
-    }
+  display.value = "";
+}
 
 function calculate() {
-    display.value = eval(display.value); 
-   }
+  display.value = eval(display.value);
+}
